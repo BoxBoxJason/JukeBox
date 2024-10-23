8d4dc52e1a5de44da5d507e24d8079c0e063243b
BoxBoxJason <contact@boxboxjason.dev>
2024-10-24 01:42:28 +0200

    Add messages routes

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,6 +2,11 @@ package api
 
 import "github.com/go-chi/chi/v5"
 
+const (
+	ID_PARAM          = "id"
+	ID_PARAM_ENDPOINT = "/{" + ID_PARAM + "}"
+)
+
 // ApiRouter creates the API router with all the routes
 func ApiRouter() chi.Router {
 	api_router := chi.NewRouter()
