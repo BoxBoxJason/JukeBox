61a7446f0fe53217959452fca61e9797b7101aca
Mathisadi <adinolfimathis@gmail.com>
2024-11-25 13:47:41 +0100

    Fix & upgrade start scripts for Windows

--- Containerfile
+++ Containerfile
@@ -2,14 +2,15 @@ FROM alpine:latest AS build
 
 WORKDIR /opt/jukebox
 
+RUN apk add --no-cache npm go
+
 COPY ./cmd/ /opt/jukebox/cmd/
 COPY ./frontend/ /opt/jukebox/frontend/
 COPY ./internal/ /opt/jukebox/internal/
 COPY ./pkg/ /opt/jukebox/pkg/
 COPY ./go.mod /opt/jukebox/
 
-RUN apk add --no-cache npm go && \
-    npm --prefix /opt/jukebox/frontend install && \
+RUN npm --prefix /opt/jukebox/frontend install && \
     npm --prefix /opt/jukebox/frontend run build && \
     go mod tidy && \
     go build -o jukebox ./cmd/server && \
@@ -23,5 +24,7 @@ COPY --from=build /opt/jukebox/jukebox /opt/jukebox/jukebox
 COPY --from=build /opt/jukebox/frontend/dist /opt/jukebox/frontend/dist
 
 HEALTHCHECK --interval=30s --timeout=5s --start-period=5s --retries=3 CMD [ "curl", "-f", "localhost:3000/api/health" ]
+
+EXPOSE 3000
 
 CMD ["/opt/jukebox/jukebox"]
--- quickstart.ps1
+++ quickstart.ps1
@@ -3,31 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 # Define the directory variables
 $JUKEBOX_SCRIPT_DIR = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
-$JUKEBOX_FRONTEND_DIR = Join-Path -Path $JUKEBOX_SCRIPT_DIR -ChildPath "frontend"
-$JUKEBOX_BIN_DIR = Join-Path -Path $JUKEBOX_SCRIPT_DIR -ChildPath "bin"
-$JUKEBOX_MAIN_GO = Join-Path -Path $JUKEBOX_SCRIPT_DIR -ChildPath "cmd\server\main.go"
-$JUKEBOX_EXECUTABLE = Join-Path -Path $JUKEBOX_BIN_DIR -ChildPath "jukebox"
 
-# Change directory to the frontend directory
-Set-Location -Path $JUKEBOX_FRONTEND_DIR
+docker build $JUKEBOX_SCRIPT_DIR -t jukebox:dev -f Containerfile
 
-# Run npm install and build
-npm --prefix $JUKEBOX_FRONTEND_DIR install
-npm --prefix $JUKEBOX_FRONTEND_DIR run build
-
-# Change directory back to the script directory
-Set-Location -Path $JUKEBOX_SCRIPT_DIR
-
-# Tidy up Go modules
-go mod tidy
-
-# Create the bin directory if it doesn't exist
-if (!(Test-Path -Path $JUKEBOX_BIN_DIR)) {
-    New-Item -ItemType Directory -Path $JUKEBOX_BIN_DIR
-}
-
-# Build the Go application
-go build -o $JUKEBOX_EXECUTABLE $JUKEBOX_MAIN_GO
-
-# Run the jukebox application
-& $JUKEBOX_EXECUTABLE
+docker run -d -p 3000:3000 jukebox:dev
