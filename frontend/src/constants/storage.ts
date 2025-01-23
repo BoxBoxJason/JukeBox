7eb427ead38fce5e10442852727e9bc957200be2
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-23 11:36:22 +0100

    Add frontend auth functions and constants

---
+++ frontend/src/constants/storage.ts
@@ -0,0 +1,9 @@
+export const LOCAL_STORAGE_KEYS = {
+  USERNAME: 'username',
+  USER_ID: 'userId',
+}
+
+export const COOKIE_KEYS = {
+  ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
+}
