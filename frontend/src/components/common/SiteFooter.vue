beea9eb7d9cf117c77fcc5bd480f97db3813ec36
BoxBoxJason <loickgoupil@yahoo.fr>
2025-01-25 22:34:45 +0100

    Prepare templates for frontend guides

--- frontend/src/components/common/SiteFooter.vue
+++ frontend/src/components/common/SiteFooter.vue
@@ -18,7 +18,7 @@ export default defineComponent({
 </script>
 <template>
   <footer class="w-full flex justify-between items-center px-12 py-4 bg-gray-800 text-white bg-slate-800">
-    <span>©{{ currentYear() }} JukeBox</span>
+    <span>©{{ currentYear() }} <a href="/">JukeBox</a></span>
     <a href="/help">Help ?</a>
     <a href="/about">About Us</a>
   </footer>
