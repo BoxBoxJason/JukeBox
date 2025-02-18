7eb08804afb009ad254c0d6ccff4370d9894fe4c
Mathisadi <adinolfimathis@gmail.com>
2025-02-18 15:57:34 +0100

    About us !!!!

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -38,82 +38,106 @@ export default defineComponent({
 <template>
   <div class="flex">
     <!-- Main Content -->
-    <main class="flex flex-col min-h-screen">
-      <div class="flex flex-col gap-4 mx-8 my-8 text-[var(--color-text-2)]">
-        <!-- Header -->
-        <div class="flex flex-row items-center gap-auto pr-4">
-          <h1 class="flex-none text-3xl">
-            <span class="text-[var(--color-heading)]">Juke</span
-            ><span class="text-[var(--color-heading-2)]">Box</span>
-          </h1>
-          <div class="flex-grow"></div>
-          <a
-            href="/"
-            class="flex flex-row p-2 rounded-full border bg-[var(--color-button-color)] flex-none text-[var(--color-text-2)] h-full items-center gap-2 hover:text-[var(--color-heading)]"
-            ><HomeIcon />Go Home</a
-          >
-        </div>
-        <hr />
+    <main class="flex flex-col gap-4 mx-8 my-8 text-[var(--color-text-2)] min-h-screen">
+      <!-- Header -->
+      <div class="flex flex-row items-center gap-auto pr-4">
+        <h1 class="flex-none text-3xl">
+          <span class="text-[var(--color-heading)]">Juke</span
+          ><span class="text-[var(--color-heading-2)]">Box</span>
+        </h1>
+        <div class="flex-grow"></div>
+        <a
+          href="/"
+          class="flex flex-row p-2 rounded-full border bg-[var(--color-button-color)] flex-none text-[var(--color-text-2)] h-full items-center gap-2 hover:text-[var(--color-heading)]"
+          ><HomeIcon />Go Home</a
+        >
+      </div>
+      <hr />
 
-        <p>
-          JukeBox is an interactive music creation platform that allows users to create music
-          together by interacting with an AI model in real time.
-        </p>
-        <p>This project is powered by three distinct AI models:</p>
-        <br />
+      <!-- Header Description -->
+      <p>
+        JukeBox is an interactive music creation platform that allows users to create music together
+        by interacting with an AI model in real time.
+      </p>
+      <p>This project is powered by three distinct AI models:</p>
+      <br />
 
-        <h2 class="text-xl">Music generation</h2>
-        <hr />
+      <!-- Models Description -->
+      <h2 class="text-xl">Music generation</h2>
+      <hr />
+      <!-- Music generation AI model -->
+      <p>
+        The music generation AI model is
+        <a
+          href="https://forum.ircam.fr/projects/detail/rave-vst/"
+          class="text-[var(--color-heading)]"
+          >RAVE</a
+        >.
+      </p>
+      <p>
+        This model is an auto encoder, meaning that is takes sound(s) as input and generates a new
+        sound as output. It can generate music in real time based on the user's commands. It fits
+        our use case perfectly, as it can generate music in real time, and the music's parameters
+        can be controlled by the user's commands.
+      </p>
+      <p>
+        You can check
+        <a href="https://github.com/acids-ircam/RAVE" class="text-[var(--color-heading)]"
+          >their GitHub repository</a
+        >
+        for more information.
+      </p>
+      <br />
 
-        <p>
-          The music generation AI model is
-          <a
-            href="https://forum.ircam.fr/projects/detail/rave-vst/"
-            class="text-[var(--color-heading)]"
-            >RAVE</a
-          >.
-        </p>
-        <p>
-          This model is an auto encoder, meaning that is takes sound(s) as input and generates a new
-          sound as output. It can generate music in real time based on the user's commands. It fits
-          our use case perfectly, as it can generate music in real time, and the music's parameters
-          can be controlled by the user's commands.
-        </p>
-        <p>
-          You can check
-          <a href="https://github.com/acids-ircam/RAVE" class="text-[var(--color-heading)]"
-            >their GitHub repository</a
-          >
-          for more information.
-        </p>
-        <br />
+      <!-- User requests processing -->
+      <h2 class="text-xl">Filtering</h2>
+      <hr />
+      <p>
+        The users chat messages are analyzed by an AI model that can filter out messages that are
+        not related to music creation, and can also understand the user's requests and translate
+        them into music commands. This model's mission is to protect the music generation AI model
+        from being spammed with irrelevant messages. It also recognizes malicious intents and can
+        ban users from the platform.
+      </p>
+      <p>
+        The filter model is a custom trained version of
+        <a
+          href="https://huggingface.co/docs/transformers/model_doc/distilbert"
+          class="text-[var(--color-heading)]"
+          >DistilBERT</a
+        >
+        trained on a dataset of music-related messages.
+      </p>
+      <br />
 
-        <h2 class="text-xl">Filtering</h2>
-        <hr />
-        <p>
-          The users chat messages are analyzed by an AI model that can filter out messages that are
-          not related to music creation, and can also understand the user's requests and translate
-          them into music commands. This model's mission is to protect the music generation AI model
-          from being spammed with irrelevant messages. It also recognizes malicious intents and can
-          ban users from the platform.
-        </p>
-        <p>
-          The filter model is a custom trained version of
-          <a
-            href="https://huggingface.co/docs/transformers/model_doc/distilbert"
-            class="text-[var(--color-heading)]"
-            >DistilBERT</a
-          >
-          trained on a dataset of music-related messages.
-        </p>
-        <br />
+      <!-- User requests processing -->
+      <h2 class="text-xl">Reformulation</h2>
+      <hr />
+      <p>
+        The reformulation model converts a user request into a music command that can be understood
+        by the music generation AI model.
+      </p>
 
-        <h2 class="text-xl">Reformulation</h2>
-        <hr />
-        <p>
-          The reformulation model converts a user request into a music command that can be
-          understood by the music generation AI model.
-        </p>
+      <!-- Team -->
+      <h2 class="text-xl">The team</h2>
+      <hr />
+      <div class="flex flex-row gap-20">
+        <figure class="flex flex-col items-center gap-2">
+          <img src="@/components/img/Mathis.jpg" class="rounded-full h-40" />
+          <figcaption>Mathis Front</figcaption>
+        </figure>
+        <figure class="flex flex-col items-center gap-2">
+          <img src="@/components/img/Thomas.jpg" class="rounded-full h-40" />
+          <figcaption>Thomas Front</figcaption>
+        </figure>
+        <figure class="flex flex-col items-center gap-2">
+          <img src="@/components/img/Lockin.jpg" class="rounded-full h-40" />
+          <figcaption>Lockin PDG</figcaption>
+        </figure>
+        <figure class="flex flex-col items-center gap-2">
+          <img src="@/components/img/Sofiane.jpg" class="rounded-full h-40" />
+          <figcaption>Sofiane Back</figcaption>
+        </figure>
       </div>
     </main>
   </div>
